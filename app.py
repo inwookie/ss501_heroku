922dc04221b7cf82f2878640bc201b64dba997f7
inwookie <inwook@berkeley.edu>
2022-01-10 13:52:46 +0900

    Update heroku deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from apps.ocr import ocr_v2, ocr_about
 from apps.main_page import main_page
 from apps.scratch_segmentation import scratch_about, scratch
 from apps.dent_detection import dent_about
-from apps.used_car import used_car_prediction, used_car_prediction_about
+from apps.used_car import used_car_prediction_about
 from apps.team import about_us
 
 
@@ -23,7 +23,7 @@ app.add_app("🚓 Number Plate Detection", ocr_v2.app)
 app.add_app("______About Number Plate Detection", ocr_about.app)
 app.add_app("🚐  Scratch Detection", scratch.app)
 app.add_app("______About Scratch Detection", scratch_about.app)
-app.add_app("🚙 Used Car Price Prediction", used_car_prediction.app)
+# app.add_app("🚙 Used Car Price Prediction", used_car_prediction.app)
 app.add_app("______About Used Car Price Prediction",
             used_car_prediction_about.app)
 app.add_app("🚐  About Dent Detection", dent_about.app)
